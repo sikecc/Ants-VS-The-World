f1ec972218c7b5a37347571d875502cb10676138
zrong5 <zlang_@hotmail.com>
2020-03-31 22:57:44 -0500

    fixed character controls and added scene transitions

---
+++ AntVsTheWorld/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    [SerializeField] private string scene;
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
---
+++ AntVsTheWorld/Assets/Scripts/SpawnPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SpawnPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject character;
+    public float x;
+    public float y;
+    public float z;
+    void Start()
+    {
+        Instantiate(character, new Vector3(x, y, z), Quaternion.identity);
+    }
+}
