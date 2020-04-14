880ca9c9658ff982aeffd3b7636e59224239cdab
clopez70 <clopez70@uic.edu>
2020-04-14 16:52:34 -0500

    Added lives

---
+++ AntVsTheWorld/Assets/Scripts/LifeManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeManager : MonoBehaviour
+{
+    public int startingLives;
+    private int lifeCounter;
+
+    private Text theCounterText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theCounterText = GetComponent<Text>();
+        lifeCounter = startingLives;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theCounterText.text = "x " + lifeCounter;
+    }
+
+    public void GiveLife()
+    {
+        lifeCounter++;
+    }
+
+    public void TakeLife()
+    {
+        lifeCounter--;
+    }
+}
