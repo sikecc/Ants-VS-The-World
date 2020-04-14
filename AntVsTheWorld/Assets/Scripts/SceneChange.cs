3064b7d21ac80b0e57a6a93f79854938e0d61f4f
zrong5 <zlang_@hotmail.com>
2020-04-14 15:19:44 -0500

    Added scene transition images

--- AntVsTheWorld/Assets/Scripts/SceneChange.cs
+++ AntVsTheWorld/Assets/Scripts/SceneChange.cs
@@ -1,14 +1,23 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneChange : MonoBehaviour
 {
     [SerializeField] private string scene;
     private AudioSource audio;
+    public RawImage transitionImage;
+    public Canvas canvas;
     private void OnTriggerEnter(Collider other)
     {
-        audio = GetComponent<AudioSource>();
-        audio.Play();
-        SceneManager.LoadScene(scene);
+        if(other.gameObject.tag == "Player")
+        {
+            audio = GetComponent<AudioSource>();
+            audio.Play();
+            RawImage image = Instantiate(transitionImage) as RawImage;
+            image.transform.SetParent(canvas.transform);
+            SceneManager.LoadScene(scene);
+        }
     }
 }
