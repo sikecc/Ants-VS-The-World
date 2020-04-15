b30085ac4efd074d5c3103bd720d01ba51c6779b
clopez70 <clopez70@uic.edu>
2020-04-14 20:08:22 -0500

    added the lives and game over menu

---
+++ AntVsTheWorld/Assets/Scripts/LifeManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeManager : MonoBehaviour
+{
+    public int startingLives;
+    private int lifeCounter;
+
+    public GameObject gameOverScreen;
+    public Player player;
+
+    private Text theCounterText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theCounterText = GetComponent<Text>();
+        lifeCounter = startingLives;
+
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lifeCounter < 0)
+        {
+            gameOverScreen.SetActive(true);
+            player.gameObject.SetActive(false);
+        }
+        theCounterText.text = "x " + lifeCounter;
+    }
+
+    public void GiveLife()
+    {
+        lifeCounter++;
+    }
+
+    public void TakeLife()
+    {
+        lifeCounter--;
+    }
+}
