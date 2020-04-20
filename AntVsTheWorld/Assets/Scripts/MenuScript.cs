bd5bcb4db8d71e853d728f2289b422dbbc9e3fe7
pguera3 <pguera3@uic.edu>
2020-04-20 18:16:10 -0500

    Added menus, tutorial, & credits

---
+++ AntVsTheWorld/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("ForestScene");
+    }
+
+    public void openTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+        
+    }
+    public void quit()
+    {
+        Debug.Log("Player has quit the game");
+        Application.Quit();
+    }
+}
---
+++ AntVsTheWorld/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }   
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        AudioListener.pause = false;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        AudioListener.pause = true;
+        isPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
