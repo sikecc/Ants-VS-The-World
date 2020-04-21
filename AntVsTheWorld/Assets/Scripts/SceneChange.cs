5b742a2c59f9d765b75d404b4469d8e57d71dc90
zrong5 <zlang_@hotmail.com>
2020-04-21 17:56:30 -0500

    Revert "unnessacary changes"
    This reverts commit 1d444751ab0461a627a93f750aea4624711a99e6.

--- AntVsTheWorld/Assets/Scripts/SceneChange.cs
+++ AntVsTheWorld/Assets/Scripts/SceneChange.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -7,8 +6,6 @@ public class SceneChange : MonoBehaviour
 {
     [SerializeField] private string scene;
     private AudioSource audio;
-    public Animator transition;
-    public float transitionTime = 1.0f;
     //public RawImage transitionImage;
     //public Canvas canvas;
     private void OnTriggerEnter(Collider other)
@@ -21,20 +18,12 @@ public class SceneChange : MonoBehaviour
             //image.transform.SetParent(canvas.transform);
 
             //Invoke("changeScene", 5.0f);
-            LoadNextLevel();
+            changeScene();
         }
     }
 
-    private void LoadNextLevel()
+    private void changeScene()
     {
-        StartCoroutine(LoadLevel());
-    }
-
-    private IEnumerator LoadLevel()
-    {
-        Debug.Log("setting trigger");
-        transition.SetTrigger("Start");
-        yield return new WaitForSeconds(transitionTime);
         SceneManager.LoadScene(scene);
     }
 }
